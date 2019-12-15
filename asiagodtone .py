9a45cb281e58bfbced36d626ea3420a6e7b492f7
kevinzoids <49157963+kevinzoids@users.noreply.github.com>
2019-12-15 20:09:54 +0800

    Update asiagodtone .py

--- asiagodtone .py
+++ asiagodtone .py
@@ -14,18 +14,18 @@ pg.display.set_caption("asiagodtone's game")
 bg = pg.Surface(screen.get_size())
 bg = bg.convert()
 bg.fill((255,255,255))
-text = font.render("Press Left Click Button to Start!", True, (0,0,0), (255,255,255))
+text = font.render("Press Left Click Button to start game!", True, (0,0,0), (255,255,255))
 bg.blit(text, (50,50))
 
 #亞統
-ball = pg.image.load("德克斯特.jpg").convert()
+ball = pg.image.load("C:\\Users\kevin\Desktop\德克斯特.jpg").convert()
 ball = pg.transform.scale(ball, (64,64))
 rect = ball.get_rect()
 rect.center = (750,450)                 
 clock = pg.time.Clock()
 
 #trash can
-can = pg.image.load("trash_with_liner.jpg").convert()
+can = pg.image.load("C://Users/kevin/Desktop/trash_with_liner.jpg").convert()
 can = pg.transform.scale(can, (128,128))
 can_rect = can.get_rect()
 can_rect.center = (random.randint(100,850),856)
@@ -39,21 +39,49 @@ running = True
 playing = True  #開始時球不能移動
 throwing = False
 stop = True
+timerON = False
+timer = [pg.time.get_ticks(), pg.time.get_ticks()]
+timeUse = timer[-1] - timer[0]
 while running:
-    clock.tick(30)  #每秒執行30次
+    clock.tick(60)  #每秒執行30次
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.MOUSEBUTTONDOWN:
             buttons = pg.mouse.get_pressed()
+            
             if buttons[0]:          #按滑鼠左鍵後球可移動
                 playing = True
                 throwing = False
                 stop = True
+                timerON = True
+
         elif event.type == pg.MOUSEBUTTONUP:
             playing = False
             throwing = True
             stop = False
+            
+########################################################################################
+    if timerON == True:
+        timer[-1] = pg.time.get_ticks()
+        timeUse = timer[-1] - timer[0]
+        #text = font.render("Press Left Click Button to start game!", True, (255,255,255), (255,255,255))
+        bg.blit(text, (50,50))
+        screen.blit(bg, (0,0))
+        SCORE = "Score: " + str(score)
+        text = font.render(SCORE, True, (0,0,0), (255,255,255))
+        TIME = font.render("Time: " + str(timeUse/1000)+" sec", True, (0,0,0), (255,255,255))
+        bg.blit(TIME, (50,150))
+        bg.blit(text, (50,50))
+        screen.blit(bg, (0,0))
+        screen.blit(ball, rect.topleft) 
+        screen.blit(can, can_rect.topleft)
+        pg.display.update()
+    else:
+        timer[0] = pg.time.get_ticks()
+            
+            
+            
 #######################################################################################
     if playing == True:
         mouses = pg.mouse.get_pos()  #取得滑鼠坐標
@@ -83,22 +111,25 @@ while running:
                     rect.centery = 888
                     score += 1
                     SCORE = "Score: " + str(score)
-                    text = font.render("Press Left Click Button to Start!", True, (255,255,255), (255,255,255))
+                    text = font.render("Press Left Click Button to start game!", True, (255,255,255), (255,255,255))
                     bg.blit(text, (50,50))
                     screen.blit(bg, (0,0))
                     text = font.render(SCORE, True, (0,0,0), (255,255,255))
+                    TIME = font.render("Time: " + str(timeUse/1000)+" sec", True, (0,0,0), (255,255,255))
+                    bg.blit(TIME, (50,150))
                     bg.blit(text, (50,50))
                     screen.blit(bg, (0,0))
                     screen.blit(ball, rect.topleft) 
                     screen.blit(can, can_rect.topleft)
                     pg.display.update()
                     can_rect.center = (random.randint(100,850),856)
-
+                    
+                    timerON = False
 ########################################################################################
 
+
+
+
 pg.quit()        
-
-
-
 
 
